de6264dfb6adf9f2bb9f99c0fa50d2d70e31cc44
Anh-Kagi <anh.kagi@protonmail.com>
2021-11-02 16:30:12 +0100

    spring-mvc starter
    Signed-off-by: Anh-Kagi <anh.kagi@protonmail.com>

---
+++ NaturalTheScattering/src/main/java/fr/polytech/projet/naturalthescattering/NaturalTheScattering/NaturalTheScatteringApplication.java
@@ -0,0 +1,12 @@
+package fr.polytech.projet.naturalthescattering.NaturalTheScattering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaturalTheScatteringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaturalTheScatteringApplication.class, args);
+	}
+}
---
+++ NaturalTheScattering/src/test/java/fr/polytech/projet/naturalthescattering/NaturalTheScattering/NaturalTheScatteringApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.polytech.projet.naturalthescattering.NaturalTheScattering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaturalTheScatteringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
