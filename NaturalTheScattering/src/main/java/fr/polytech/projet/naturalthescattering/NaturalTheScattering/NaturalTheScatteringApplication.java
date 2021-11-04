a15fbde501d32be6bec8bd9d3419787eec6e97d0
Anh-Kagi <anh.kagi@protonmail.com>
2021-11-04 15:16:08 +0100

    created db test
    Signed-off-by: Anh-Kagi <anh.kagi@protonmail.com>

--- NaturalTheScattering/src/main/java/fr/polytech/projet/naturalthescattering/NaturalTheScattering/NaturalTheScatteringApplication.java
+++ NaturalTheScattering/src/main/java/fr/polytech/projet/naturalthescattering/NaturalTheScattering/NaturalTheScatteringApplication.java
@@ -1,12 +1,8 @@
 package fr.polytech.projet.naturalthescattering.NaturalTheScattering;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import fr.polytech.projet.naturalthescattering.NaturalTheScattering.db.Joueur;
 
 @SpringBootApplication
 public class NaturalTheScatteringApplication {
@@ -15,18 +11,5 @@ public class NaturalTheScatteringApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(NaturalTheScatteringApplication.class, args);
-	}
-	
-	@Bean
-	public CommandLineRunner dbdemo() {
-		return (args) -> {
-			Joueur pedro = new Joueur("XxxPedroLePloxxX", "NotAPlo", 1000000000);
-			Joueur pablo = new Joueur("XxxPabloLePloxxX", "IsAPlo", -1000000000);
-			//repo.joueurs.save(pedro);
-			repo.joueurs.save(pablo);
-			
-			pedro.createGuilde("Ordre");
-			repo.joueurs.save(pedro);
-		};
 	}
 }
--- NaturalTheScattering/src/test/java/fr/polytech/projet/naturalthescattering/NaturalTheScattering/NaturalTheScatteringApplicationTests.java
+++ NaturalTheScattering/src/test/java/fr/polytech/projet/naturalthescattering/NaturalTheScattering/NaturalTheScatteringApplicationTests.java
@@ -1,13 +1,22 @@
 package fr.polytech.projet.naturalthescattering.NaturalTheScattering;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import fr.polytech.projet.naturalthescattering.NaturalTheScattering.db.Joueur;
 
 @SpringBootTest
 class NaturalTheScatteringApplicationTests {
+	@Autowired
+	Repository repo;
 
 	@Test
-	void contextLoads() {
+	public void db_test() {
+		Joueur pedro = new Joueur("XxxPedroLePloxxX", "NotAPlo", 1000000000);
+		Joueur pablo = new Joueur("XxxPabloLePloxxX", "IsAPlo", -1000000000);
+			
+		pedro.createGuilde("Ordre");
+		repo.joueurs.save(pedro);
+		repo.joueurs.save(pablo);
 	}
-
 }
