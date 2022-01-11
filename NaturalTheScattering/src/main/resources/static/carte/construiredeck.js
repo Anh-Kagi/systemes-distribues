9021aa3690493246807761011aa210f28de36b12
Anh-Kagi <anh.kagi@protonmail.com>
2022-01-11 21:02:42 +0100

    moved source files
    Signed-off-by: Anh-Kagi <anh.kagi@protonmail.com>

--- NaturalTheScattering/src/main/resources/static/carte/construiredeck.js
+++ NaturalTheScattering/src/main/resources/static/carte/construiredeck.js
@@ -1,16 +1,26 @@
-function nbCard(){
+"use strict";
+
+Vue.component('deck-card', {
+  props: ['card'],
+  template: "<button type="
+  button " class="
+  btn btn - primary btn - lg m - 2 " onClick="
+  ">{{card.name}}</button>"
+});
+
+function nbCard() {
   return document.getElementById('dropzone').childElementCount;
 }
 
-function addButton (name){
+function addButton(name) {
   var btn = document.createElement("BUTTON");
   var t = document.createTextNode(name);
   btn.appendChild(t);
   document.body.appendChild(btn);
 }
 
-function deckVerification (){
-  if (nbCard()==20){
-    JavaScript:window.close();
+function deckVerification() {
+  if (nbCard() == 20) {
+    JavaScript: window.close();
   }
 }
