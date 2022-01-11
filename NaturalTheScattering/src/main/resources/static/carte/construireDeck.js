c0b79c4139db12c2f188e3a2f83c968cdc23d17f
jimmy789521 <71819826+jimmy789521@users.noreply.github.com>
2022-01-11 20:39:16 +0100

    Mise à jour graphique

---
+++ NaturalTheScattering/src/main/resources/static/carte/construireDeck.js
@@ -0,0 +1,24 @@
+"use strict";
+
+Vue.component('deck-card',{
+  props:['card'],
+  template:
+  "<button type="button" class="btn btn-primary btn-lg m-2" onClick="">{{card.name}}</button>"
+});
+
+function nbCard(){
+  return document.getElementById('dropzone').childElementCount;
+}
+
+function addButton (name){
+  var btn = document.createElement("BUTTON");
+  var t = document.createTextNode(name);
+  btn.appendChild(t);
+  document.body.appendChild(btn);
+}
+
+function deckVerification (){
+  if (nbCard()==20){
+    JavaScript:window.close();
+  }
+}
